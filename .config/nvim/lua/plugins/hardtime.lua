5263bc887658bcdaa2056fcc35231c5137787833
Andrew Ikovic <andrewikovic@node-1w7jra220hoxo15anua7zoy5u.ipv6.telus.net>
2026-06-30 01:17:12 -0600

    Add nvim config

---
+++ .config/nvim/lua/plugins/hardtime.lua
@@ -0,0 +1,6 @@
+-- ~/.config/nvim/lua/plugins/hardtime.lua
+return {
+  "m4xshen/hardtime.nvim",
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+}
---
+++ .config/nvim/lua/plugins/modus.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      style = "modus_vivendi",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "modus",
+    },
+  },
+}
