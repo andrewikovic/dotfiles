5b8e218167270a5e89c1da90111583d412c3077f
Andrew Ikovic <andrewikovic@node-1w7jra220hoxo15anua7zoy5u.ipv6.telus.net>
2026-06-30 01:16:44 -0600

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+alias dotfiles='/usr/bin/git --git-dir=/home/andrewikovic/.dotfiles/ --work-tree=/home/andrewikovic'
